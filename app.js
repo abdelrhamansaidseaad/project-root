5cbc64a010dd4d17d8b6780766e132ae7d4a4e21
Abdulrahman Saeed Saad <abdulrhaman377@gmail.com>
2025-05-03 20:43:15 +0300

    first commit

--- app.js
+++ app.js
@@ -12,7 +12,11 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
